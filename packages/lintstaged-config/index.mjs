8783c7306193bdb665907541292f5700d752a8d0
DrJume <12785972+DrJume@users.noreply.github.com>
2023-12-07 18:14:46 +0100

    feat(lintstaged): add configuration option for running inside monorepo package

--- packages/lintstaged-config/index.mjs
+++ packages/lintstaged-config/index.mjs
@@ -1,7 +1,12 @@
-export default {
-  '*.{vue,?([cm])[jt]s?(x),y?(a)ml,json?(c),md,html,?(s)css}': [
-    'eslint --fix --cache',
-    'prettier --write --cache',
-  ],
-  '*.{vue,?([cm])ts?(x)}': () => 'vue-tsc -p tsconfig.json --noEmit --composite false', // run once for all files
+/** @param {string} [runInPackage] */
+export default (runInPackage) => {
+  const pnpmExec = runInPackage ? `pnpm --filter ${runInPackage} exec ` : ''
+
+  return {
+    '*.{vue,?([cm])[jt]s?(x),y?(a)ml,json?(c),md,html,?(s)css}': [
+      `${pnpmExec}eslint --fix --cache`,
+      `${pnpmExec}prettier --write --cache`,
+    ],
+    '*.{vue,?([cm])ts?(x)}': () => `${pnpmExec}vue-tsc -p tsconfig.json --noEmit --composite false`, // run once for all files
+  }
 }
