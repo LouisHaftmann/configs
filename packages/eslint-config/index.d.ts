2fa57c10c30cde8cd3daa1a70005ac74f40fe276
Julian Meinking <12785972+DrJume@users.noreply.github.com>
2024-08-12 22:48:55 +0200

    refactor: remove unused unicorn parameter type

--- packages/eslint-config/index.d.ts
+++ packages/eslint-config/index.d.ts
@@ -2,7 +2,6 @@ import type antfu from '@antfu/eslint-config'
 
 declare function eslintConfig(config: {
   nuxt: boolean
-  unicorn?: boolean
   tsconfigPath?: string
 }): ReturnType<typeof antfu>
 
